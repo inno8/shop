f6dfeda17ae645f4b27c556bde4c004c30504d19
inno8 <innovale@kakao.com>
2023-01-25 07:51:23 +0900

    2023.01.25 박중선 Item class 멤버 변수 선언

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,38 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    //상품 코드
+    private Long id;
+
+    //상품 이름
+    private String itemName;
+
+    //가격
+    private int price;
+
+    //재고 수량
+    private int stockNumber;
+
+    //상품 상세 설명
+    private String itemDetail;
+
+    //상품 판매 상태
+    private ItemSellStatus itemSellStatus;
+
+    //등록 시간
+    private LocalDateTime regTime;
+
+    //수정 시간
+    private LocalDateTime udtTime;
+}
